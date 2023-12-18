245eb4cc585db832b1c820510ee8fcee316932f3
dhk527 <dhk_0527@naver.com>
2023-12-18 15:06:39 +0900

    1-D plus

--- practice_1217/DCT.c
+++ practice_1217/DCT.c
@@ -13,6 +13,75 @@ float getCval(int x, int y){
 		return sqrt (2.0f/1.0f*N); 
 
 }  
+
+void DCT1d(unsigned char buffer[ROWS][COLS], float result[ROWS*COLS]){
+
+    if(buffer == NULL){
+	perror("error\n");
+    }
+
+    printf ("\n========================================================\n");
+
+    int u, v, i, j;
+    int m = ROWS, n = COLS;
+    float k;
+    float csum = 0.0f;
+    float iresult[ROWS*COLS]; 
+    float change_buffer_float[ROWS*COLS];
+
+  for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            change_buffer_float[i*n+j] = buffer[i][j];
+        printf("%.1f ",change_buffer_float[i*n+j]); // 2D를 1D로 변환완료.
+	}
+  }
+   
+  printf("\n\nDCT 1-Dimension  변환 시 : \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+	   csum = 0;
+	
+	       for (u = 0; u < m; u++)
+                for (v = 0; v < n; v++)
+                    csum += change_buffer_float[u*n + v]
+                        *cos((2 * u + 1) * i * M_PI / (2 * m))
+                        *cos((2 * v + 1) * j * M_PI / (2 * n));
+	    
+	    if (!i && !j) k = 0.5;
+            else if ((!i && j) || (i && !j)) k = 1 / sqrt(2);
+            else k = 1;
+
+            result[i * n + j] = (2 * csum * k) / n;
+            printf("%.1f ", result[i*n + j]); // fraction 1자리
+       	}
+        printf("\n");
+    }    
+    printf("\nInverse DCT 1-Dimension  변환시 \n");
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+	    csum = 0;
+
+            for (u = 0; u < m; u++)
+                for (v = 0; v < n; v++) {
+                    if (!u && !v) k = 0.5;
+                    else if ((!u && v) || (u && !v)) k = 1 / sqrt(2);
+                    else k = 1;
+                    csum += k * result[u * n + v]
+                        * cos((2 * i + 1) * M_PI * u / (2 * m))
+                        * cos((2 * j + 1) * M_PI * v / (2 * n));
+                }
+            iresult[i * n + j] =  csum *(2.0 / n);
+
+            printf("%0.1f ", iresult[i * n + j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+
 void DCT(unsigned char buffer[ROWS][COLS], float result[ROWS][COLS]){
 
 	if(buffer == NULL){
@@ -29,6 +98,7 @@ void DCT(unsigned char buffer[ROWS][COLS], float result[ROWS][COLS]){
 	printf("\nDCT 변환 시 : \n");
 	for(i = 0;i < m;i++){
 		for(j = 0;j < n;j++){
+
 			for(u = 0;u < m;u++)
 				for(v = 0;v < n;v++)
 					csum += buffer[u][v]
@@ -38,6 +108,7 @@ void DCT(unsigned char buffer[ROWS][COLS], float result[ROWS][COLS]){
 			else if((!i && j) || (i && !j)) k = 1/sqrt(2);
 			else k = 1;
 			result[i][j] = 2*csum*k/n;
+
 			printf("%.1f ",result[i][j]);//fraction 1자리
 			csum = 0;
 		}
@@ -68,10 +139,16 @@ void DCT(unsigned char buffer[ROWS][COLS], float result[ROWS][COLS]){
 
 void  DCT_stdout(unsigned char buffer[ROWS][COLS]){
 
+
+#ifdef DCT_1d
+	
+	float result[ROWS*COLS];
+        DCT1d(buffer, result);
+
+#else
 	float result[ROWS][COLS];
-
 	DCT(buffer, result); // buffer는 fread한 주소 가리킴 , result는 DCT 변환 된 것
-
+#endif
 	printf("\ncomplete DCT transfomation\n");
 
 
--- practice_1217/DCT.h
+++ practice_1217/DCT.h
@@ -1,11 +1,12 @@
-#ifndef DCT_origin_H
-#define DCT_origin_H
+#ifndef _DCT_H_
+#define _DCT_H_
 
 #define ROWS 16
 #define COLS 16
 #define N 16 
 
 void DCT(unsigned char buffer[ROWS][COLS], float result[ROWS][COLS]);
+void DCT1d(unsigned char buffer[ROWS][COLS], float result[ROWS*COLS]);
 void DCT_stdout(unsigned char buffer[ROWS][COLS]);
 
 #endif
