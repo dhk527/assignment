e3ad0161b4ad764530a6c2d422f676673d384c56
dhk527 <dhk_0527@naver.com>
2024-02-05 17:53:18 +0900

    0205

---
+++ RECUR/GDC.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void GDC(int* numerator, int* denominator)
+{
+	int remainder=1;
+
+	remainder = (*numerator) % (*denominator); // 유클리드 호제법
+	*numerator = *denominator;
+	
+	if (remainder==0)
+	{
+	printf("%d\n",(*denominator));
+	return ;
+	}
+
+	*denominator=remainder;
+
+
+	GDC(numerator, denominator); // 재귀호출
+}
+	
+
+
+int main()
+{	
+	int numerator;
+	int denominator;
+
+	puts("first");
+	scanf("%d",&numerator);
+	puts("second");
+	scanf("%d",&denominator);
+
+	GDC(&numerator, &denominator);
+
+}
