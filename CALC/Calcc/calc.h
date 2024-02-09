f6940075c5adeadf8e5a141ce676e07cbc2605f3
root <root@KDH.localdomain>
2024-02-09 13:28:34 +0900

    0209

--- CALC/Calcc/calc.h
+++ CALC/Calcc/calc.h
@@ -3,11 +3,11 @@
 typedef double (*calc)(double, double);
 //만약, typedef 안할 시, double (*calc)(double, double)을 써야함.
 
-extern double Add(double, double);
-extern double Sub(double, double);
-extern double Div(double, double);
-extern double Mul(double, double);
+double Add(double, double);
+double Sub(double, double);
+double Div(double, double);
+double Mul(double, double);
 //extern double call_calc(double (*calc)(double, double), double, double);
-extern void call_calc(calc, double, double);
+void call_calc(calc, double, double);
 
 #endif
--- CALC/Calcc/ui.h
+++ CALC/Calcc/ui.h
@@ -3,8 +3,8 @@
 
 //typedef enum MENU {EXIT, add, sub, div, mul} MENU;
 
-extern void SetData(double*, double*);
-extern void Set_Operand(char*,double*, double*);
-extern void EventLoop();
+void SetData(double*, double*);
+void Set_Operand(char*,double*, double*);
+void EventLoop();
 #endif
 
