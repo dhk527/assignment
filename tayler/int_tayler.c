08274937fe94a54e7953ffd3c3e31bb612c09657
dhk527 <dhk_0527@naver.com>
2024-05-01 19:39:04 +0900

    0501

---
+++ tayler/int_tayler.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+#define PI 3.14159265358979323846
+
+int factorial(int a)
+{
+	for(int i=1; i<a+1; i++)
+		a*=i;	
+	return a;
+}
+
+
+int main()
+{
+	int 
+
+
+}
